b629a40e7dc9d0daaf0cad11c21bc563b9e641a3
Alex Leontiev <alozz1991@gmall.com>
2018-10-24 11:57:44 +0900

    gym manager files changed:  M src/main/java/managers/GymManager.java
    trello card: https://trello.com/c/AFd5TXE3

--- src/main/java/managers/GymManager.java
+++ src/main/java/managers/GymManager.java
@@ -1,17 +1,27 @@
 package managers;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.mongodb.MongoClient;
+
+import util.MongoUtil;
 
 import static java.util.Arrays.asList;
 import static util.parsers.StandardParser.ArgTypes;
 
+import java.util.logging.Logger;
+
 public class GymManager extends AbstractManager {
 	private MongoClient mongoClient_;
+	private JSONObject gymSingleton_;
+	private Logger logger_;
 
-	public GymManager(MongoClient mongoClient) {
+	public GymManager(MongoClient mongoClient) throws Exception {
 		mongoClient_ = mongoClient;
+		logger_ = Logger.getLogger(this.getClass().getName());
+		gymSingleton_ = MongoUtil.GetJsonObjectFromDatabase(mongoClient_, "logistics.gymSingleton");
+		logger_.info(String.format("gymSingleton_=%s", gymSingleton_.toString()));
 	}
 
 	@Override
