bea39d953ade2abe76e334728972981b49ffe127
Alex Leontiev <alozz1991@gmall.com>
2018-11-29 12:02:27 +0900

    GF: task tracker files changed:  M src/main/scriptpl/build.pl  M trello.json
    trello card: https://trello.com/c/NSzDangD

--- src/main/scriptpl/build.pl
+++ src/main/scriptpl/build.pl
@@ -21,12 +21,37 @@
 use strict;
 use warnings;
 use utf8;
+use Text::Table;
 
 
 #global const's
 my $TESTFLAG = 0;
+my $DEFMETHOD = 'help';
+my %METHODS = (
+	help => {
+		description=>"show this message",
+		function=>\&help,
+	},
+	pulljar => {
+		description => 'pulljar',
+		function => sub {
+			myExec('make pull jar');
+		}
+	}
+);
 #global var's
 #procedures
+sub help{
+	my $tb = Text::Table->new(
+		"method",\' | ',"description",
+	);
+	$tb->add('-----','-------');
+	for(keys(%METHODS)){
+		$tb->add($_,$METHODS{$_}->{description});
+	}
+#	$tb->add('-----','-------');
+	print $tb;
+}
 sub myExec{
 	(my $cmd) = @_;
 	printf(STDERR "exec: _%s\n",$cmd);
@@ -36,9 +61,12 @@ sub myExec{
 }
 #main
 (my $method) = @ARGV;
+$method //= "";
 chomp $method;
-if($method eq 'pulljar'){
-	myExec('make pull jar');
+my $func;
+if(exists $METHODS{$method}){
+	$func = $METHODS{$method}->{function};
 } else {
-	printf("unknown method %s\n",$method);
+	$func = $METHODS{$DEFMETHOD}->{function};
 }
+$func->();
