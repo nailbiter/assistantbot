bc804770b40c3d4c28861ee9cdd77aafdceb7e77
Alex Leontiev <alozz1991@gmall.com>
2018-12-12 14:00:00 +0900

    optimize files changed:  M src/main/java/managers/TaskManager.java  M src/main/java/managers/tasks/TaskManagerBase.java  M src/main/js/taskcomparators/common/common.js  M trello.json
    trello card: https://trello.com/c/cKyvGxxY

--- src/main/js/taskcomparators/common/common.js
+++ src/main/js/taskcomparators/common/common.js
@@ -1,8 +1,7 @@
 function compare(obj1,obj2){
-	//var obj1 = JSON.parse(ScriptHelper.execute("a")),
-	//	obj2 = JSON.parse(ScriptHelper.execute('b'));
 	return JSON.stringify(strcmp(obj1.name,obj2.name));
 }
+
 function strcmp(a, b)
 {   
     return (a<b?-1:(a>b?1:0));  
