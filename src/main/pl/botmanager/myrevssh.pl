6f4ee7a276b9e89ffefe49b04c62c85fc2f3460c
Alex Leontiev <alozz1991@gmall.com>
2018-12-30 16:56:40 +0900

    я в течении пары дней его подготовлю и тебе отправлю files changed:  M src/main/pl/botmanager/myrevssh.pl  M src/main/resources/profiles/interactive.json  M trello.json
    trello card: https://trello.com/c/l4X6a80c

--- src/main/pl/botmanager/myrevssh.pl
+++ src/main/pl/botmanager/myrevssh.pl
@@ -23,5 +23,24 @@ use warnings;
 use utf8;
 
 
+#global const's
+my $REMOTE = 'inp9822058@alumni.cs.nctu.edu.tw';
+my $UNIT = 10000;
+my $CERTKEY = '';
+#procedures
+sub myRand{
+	(my $a,my $b) = @_;
+	return $a+int(rand($b-$a));
+}
+
 #main
-printf('hi!\n');
+my $id = myRand(10000,20000);
+my $cmd = sprintf("daemonize ssh -R %d:localhost:22 %s",$id,$REMOTE);
+#`$cmd`;
+my $num1=myRand(1*$UNIT,2*$UNIT);
+my $num2=myRand(3*$UNIT,4*$UNIT);
+
+#echo `date` : $num1 : $num2 | ssh $CERTKEY $LOGIN 'cat >> ports.txt'
+$cmd = sprintf("daemonize /usr/bin/autossh -f -N -M %d -o \"PubkeyAuthentication=yes\" -o \"PasswordAuthentication=no\" %s -R %d %s -p 22 -vvv",$num1,$CERTKEY,$num2,$REMOTE);
+system($cmd);
+printf("ssh localhost -p %d\nssh localhost -p %d\n",$num1,$num2);
