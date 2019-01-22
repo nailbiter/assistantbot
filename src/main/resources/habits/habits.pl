aff340fd0c619491a9a800c171beb6a2d2d6a7c8
Alex Leontiev <alozz1991@gmall.com>
2019-01-22 21:12:11 +0900

    habits files changed:  M src/main/resources/habits/habits.pl  M trello.json
    trello card:

--- src/main/resources/habits/habits.pl
+++ src/main/resources/habits/habits.pl
@@ -26,7 +26,7 @@ makeHabit "6 7 * * *", "pullups 10:4", 1080, category=> "gym" ;
 makeHabit "0 19 * * fri", "clean ears", 1380 ;
 makeHabit "0 13 * * *", "elsa", 1380 ;
 makeHabit "0 12 1 * *", "back-up K emails", 1380 ;
-makeHabit "0 10 * * *", "backup baito DB twice", 1380, onFailed=>'remove';
+makeHabit "0 10 * * *", "backup baito DB twice, check apaz/dulut time", 1380, onFailed=>'remove';
 makeHabit "0 7 * * *", "morning note habit", 300 ,info=>"https://docs.google.com/document/d/1Q4wvok8I1AAc0Jtv6gU3d-MJK69uvsqCeLPbBvLiCTg/edit#";
 makeHabit "0 7 * * *", "face whipe, wash head, faceCream x 2", 300 ;
 makeHabit "0 7 * * *", "apple", 300 ;
