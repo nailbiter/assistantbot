8a32a4b33fde5774ff6cb7050d9f3d1289551787
Alex Leontiev <alozz1991@gmall.com>
2019-01-22 22:03:48 +0900

    gym progra files changed:  M src/main/resources/params/gym/common/commonProgramData.js  M trello.json
    trello card:

--- src/main/resources/params/gym/common/commonProgramData.js
+++ src/main/resources/params/gym/common/commonProgramData.js
@@ -471,5 +471,37 @@ var program = [
 			}
 		]
 	},
+	{
+		weekCount:weekShift(3),
+		dayCount:3,
+		program:[
+			{
+				name:"tricPull",
+				"reps":"3*10x*35k, 2*8x*40k"
+			},
+			{
+				name:"grud",
+				reps:"3x5x32.5"
+			},
+			{
+				name:"squat",
+				reps:[
+					['8x*5',77],
+				],
+			},
+			{
+				name:"stan",
+				reps:"3x8x50"
+			},
+			{
+				name:"matr",
+				"reps":"28.5k*12x*3"
+			},
+			{
+				name:"pullup",
+				reps:"2*Max"
+			}
+		]
+	},
 ]
 module.exports = program;
