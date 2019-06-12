02a1f342cb81799d6ef393d996b449729d223ff6
Alex Leontiev <alozz1991@gmall.com>
2019-06-12 14:58:53 +0900

    gym

--- src/main/resources/params/gym/common/commonProgramData.js
+++ src/main/resources/params/gym/common/commonProgramData.js
@@ -525,5 +525,84 @@ var program = [
 			},
 		]
 	},
+	{
+		weekCount:weekShift(4),
+		dayCount:2,
+		program:[
+			{
+				name:"brus",
+				reps:"2*12, 3*30k*8x"
+			},
+			{
+				name:"rsid",
+				reps:"2*15*12k,5x, 3*10*14k"
+			},
+			{
+				name:"bic",
+				reps:"2*15*12.5k, 3*10*15k"
+			},
+			{
+				name:"molot",
+				reps:"2*15*15k, 3*12*22.5k"
+			}
+		]
+	},
+	{
+		weekCount:weekShift(4),
+		dayCount:3,
+		program:[
+			{
+				name:"tricPull",
+				"reps":"3*10x*35k, 2*8x*40k"
+			},
+			{
+				name:"grud",
+				reps:"3x5x35"
+			},
+			{
+				name:"squat",
+				reps:[
+					['8x*2',77],
+					['6x*1',79],
+					['5x*2',81],
+				],
+			},
+			{
+				name:"stan",
+				reps:"3x8x50"
+			},
+			{
+				name:"matr",
+				"reps":"28.5k*12x*3"
+			},
+			{
+				name:"pullup",
+				reps:"2*Max"
+			}
+		]
+	},
+	{
+		weekCount:6,
+		dayCount:4,
+		program:[
+			{
+				"name":"bench",
+				"reps":[
+					['5',62.5],
+					['3',72.5],
+					['3(1*)',80.0],
+					['3*3',85.0],
+				]
+			},
+			{
+				"name":"fly",
+				"reps":"3*10-12"
+			},
+			{
+				"name":"stand press",
+				"reps":"3*6"
+			},
+		]
+	},
 ]
 module.exports = program;
