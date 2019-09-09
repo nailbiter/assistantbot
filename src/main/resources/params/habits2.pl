e22686353049811f1a9c499840cd555731076aed
Alex Leontiev <alozz1991@gmail.com>
2019-09-10 00:16:31 +0900

    habits

--- src/main/resources/params/habits2.pl
+++ src/main/resources/params/habits2.pl
@@ -1,1 +1,1 @@
-makeHabit "0 9 * * *", "test", -1, callback=>{name=>'managers.NewTrelloManager',method=>'report'},enabled=> 1, onFailed=>'putlabel' ;
+makeHabit "0 9 * * *", "test", -1, callback=>{name=>'managers.NewTrelloManager',method=>'report'},enabled=> 0, onFailed=>'putlabel' ;
